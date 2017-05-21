3b09c83f2441bd4d3ff388e8b0032b007bb06aa4
Jamie Garside <jgarside@uk.ibm.com>
2017-05-21 11:25:01 +0100

    Add in front end routing

--- app.ts
+++ app.ts
@@ -1,6 +1,9 @@
 import * as express from "express";
+import * as pubbot from "./pubbot";
+import * as pubbot_store from "./pubbot_store";
 import * as installs from "./installations";
 import * as redis from "redis";
+import * as bodyParser from "body-parser";
 
 // Sanity check configuration
 var requiredArgs = [
@@ -45,9 +48,12 @@ db.on("error", (err: any) => {
 });
 
 var instMgr = new installs.RedisInstallationManager(db, SLACK_CLIENT_ID, SLACK_CLIENT_SECRET);
+var botStore = new pubbot_store.InMemoryPubbotStore();
+var bot = new pubbot.SlackPubbot(botStore, instMgr);
 
 // Set up Express...
 var app = express();
+app.get("/oauth", bodyParser.json());
 app.get("/oauth", (req, res) => {
   // Get the parameters and proxy on...
   var code = req.query.code;
@@ -56,6 +62,36 @@ app.get("/oauth", (req, res) => {
   instMgr.provideCode(code, err => {
     if(err) return res.status(500).end(err);
     res.end("Ok!");
+  });
+});
+
+app.post("/jamiego", bodyParser.json());
+app.post("/jamiego", (req, res) => {
+  // Unpick...
+  if(!req.body) return res.status(400).end("No body sent...");
+  if(!req.body.token || req.body.token != SLACK_VERIFICATION_TOKEN) return res.status(400).end("Verification token does not match");
+
+  var team = req.body.team_id;
+  var channel = req.body.channel_id;
+
+  if(!team || !channel) return res.status(400).end("Team or channel missing from request");
+
+  bot.sendRequest(team, channel, 1, err => {
+    if(err) return res.status(500).end(err);
+  });
+});
+
+app.post("/action", bodyParser.json());
+app.post("/action", (req, res) => {
+  if(!req.body) return res.status(400).end("No body sent");
+  if(!req.body.token || req.body.token != SLACK_VERIFICATION_TOKEN) return res.status(400).end("Verification token does not match");
+
+  var team = req.body.team.id;
+  var channel = req.body.channel.id;
+  if(!team || !channel) return res.status(400).end("Team or channel missing from request");
+  
+  bot.tickRequest(team, channel, err => {
+    if(err) return res.status(500).end(err);
   });
 });
 
